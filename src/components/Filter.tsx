1ce40f2403fd60897f01cf6910da0e77916b2b50
ammarsarhan <ammarsarhan06@gmail.com>
2025-01-22 10:53:39 +0200

    Started Filter component

---
+++ src/components/Filter.tsx
@@ -0,0 +1,10 @@
+export default function Filter () {
+    return (
+        <div className="px-6 py-5 border-b-[1px]">
+            <span className="text-sm text-gray-500">Press the spacebar to generate a new set of exercises.</span>
+            <div className="flex items-center gap-x-4">
+                
+            </div>
+        </div>
+    )
+}

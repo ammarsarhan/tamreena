007f0a0e30ce36024b658dc469b26c60f755769e
ammarsarhan <ammarsarhan06@gmail.com>
2025-02-03 02:19:20 +0200

    Created email and password sign up/sign in functions

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,12 +1,28 @@
-import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { app } from './main';
 
 export const auth = getAuth(app);
 
 export async function createUserWithCredentials(email: string, password: string) {
-    
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((res) => {
+        if (!res.user) {
+            throw new Error("Could not create new user.")
+        }
+    })
+    .catch((error) => {
+        throw new Error(`Failed to create new user. (${error.message})`)
+    })
 }
 
 export async function signInUserWithCredentials(email: string, password: string) {
-    
+    signInWithEmailAndPassword(auth, email, password)
+    .then((res) => {
+        if (!res.user) {
+            throw new Error("Could not create new user.")
+        }
+    })
+    .catch((error) => {
+        throw new Error(`Failed to create new user. (${error.message})`)
+    })
 }
